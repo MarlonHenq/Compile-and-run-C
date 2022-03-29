c9ca12504e347a262bd806f3b5e84193c4a1714e
MarlonHenq <marlon@marlonhenq.dev>
2022-03-28 22:29:45 -0300

    add forced rum quest

--- crum.sh
+++ crum.sh
@@ -1,9 +1,26 @@
+#!/bin/bash
 rm a.out
 clear
+
 if gcc $1; then
     ./a.out
-elif $2 == '-f'; then
-    echo "====FORCED RUM by CRum===="
-    ./a.out
+else
+    echo "CRum Says: Compilation Error, Forced Rum? [y/n]"
+    read answer
+
+    if "$answer" = "y"; then
+
+        echo ""
+        echo "====FORCED RUM by CRum===="
+        echo ""
+
+        if a.out; then
+            ./a.out
+        else
+            echo "CRum Says: Fatal error on compilation, a.out not found."
+        fi
+        
+    fi
 fi
+
 rm a.out
